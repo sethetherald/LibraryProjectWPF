5f55f09ee659d592033aeb9577dae591ca128fef
REVISION-PC\Seth <sethetherald@duck.com>
2023-02-27 12:49:15 +0700

    Finished Login function

---
+++ LibraryProjectWPF/Ultilities/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LibraryProjectWPF.Ultilities
+{
+    internal class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+        {
+            if (execute != null)
+            {
+                _canExecute = canExecute;
+                _execute = execute;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ LibraryProjectWPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LPLibrary.DataAccess.Models;
+using System.Windows;
+
+namespace LibraryProjectWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Account CurrentAccount { get; set; }
+
+        public MainWindow(Account account, Librarian librarian)
+        {
+            InitializeComponent();
+            CurrentAccount = account;
+            tbTest.Text = "Hello " + librarian.LibrarianName;
+        }
+    }
+}
