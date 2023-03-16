2b444eafe7edbf999024efffb6dd037fdba55fcb
REVISION-PC\Seth <sethetherald@duck.com>
2023-03-16 11:37:38 +0700

    Update Main window to use binding.

--- LibraryProjectWPF/ViewModels/MainWindowViewModel.cs
+++ LibraryProjectWPF/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryProjectWPF.Utilities;
 using LibraryProjectWPF.Views;
+using LPLibrary.DataAccess.Models;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,14 +8,23 @@ namespace LibraryProjectWPF.ViewModels
 {
     internal class MainWindowViewModel
     {
+        private int _librarianId;
+        public string WelcomeText { get; set; }
+        public string ButtonVisibility { get; set; }
+        public Account CurrentAccount { get; set; }
         public ICommand BookManagerCommand { get; set; }
         public ICommand ReaderManagerCommand { get; set; }
         public ICommand LendBookManagerCommand { get; set; }
         public ICommand ReturnBookManagerCommand { get; set; }
         public ICommand AccountManagerCommand { get; set; }
 
-        public MainWindowViewModel(int librarianId)
+        public MainWindowViewModel(Account account, Librarian librarian)
         {
+            CurrentAccount = account;
+            _librarianId = librarian.LibrarianId;
+            WelcomeText = "Welcome " + librarian.LibrarianName;
+            ButtonVisibility = CurrentAccount.Role == 1 ? "Visible" : "Hidden";
+
             BookManagerCommand = new RelayCommand<object>(
                 (_) => true,
                 (_) => BookManager()
@@ -27,17 +37,17 @@ namespace LibraryProjectWPF.ViewModels
 
             LendBookManagerCommand = new RelayCommand<object>(
                 (_) => true,
-                (_) => LendBookManager(librarianId)
+                (_) => LendBookManager(_librarianId)
                 );
 
             ReturnBookManagerCommand = new RelayCommand<object>(
                 (_) => true,
-                (_) => ReturnBookManager(librarianId)
+                (_) => ReturnBookManager(_librarianId)
                 );
 
             AccountManagerCommand = new RelayCommand<object>(
                 (_) => true,
-                (_) => AccountManager(librarianId)
+                (_) => AccountManager(_librarianId)
                 );
         }
 
